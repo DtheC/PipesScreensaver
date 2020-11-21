bd951b309268821790ed4d27c05796f12d283ec5
Travis <travis@denouncetheclock.com>
2020-11-21 12:31:17 +1100

    Draw prefab cylinders as the pipes

---
+++ Assets/DrawPipePrefab.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawPipePrefab : MonoBehaviour
+{
+  public static DrawPipePrefab Instance;
+  public GameObject pipePrefab;
+
+  void Awake()
+  {
+    Instance = this;
+  }
+  public void DrawPrefab(Vector3 position, Vector3 direction, Color colour)
+  {
+    var trans = Instantiate(pipePrefab, position, Quaternion.identity).GetComponent<Transform>();
+    trans.LookAt(position + direction);
+    trans.GetComponentInChildren<MeshRenderer>().material.color = colour;
+  }
+}
