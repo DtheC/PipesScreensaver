bf2aab65be10813f5161e8724b6dbd09832d8357
Travis <travis@denouncetheclock.com>
2020-11-28 20:50:07 +1100

    Rearranging files and add start of maze

---
+++ Assets/Maze/Scripts/CharacterControls.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum Direction
+{
+  Forward,
+  Right,
+  Left,
+  Back
+};
+
+public class CharacterControls : MonoBehaviour
+{
+  bool moving;
+  public void Move(Direction movement)
+  {
+    switch (movement)
+    {
+      case Direction.Forward:
+        transform.position = transform.position + transform.forward;
+        break;
+      case Direction.Back:
+        transform.position = transform.position - transform.forward;
+        break;
+
+      case Direction.Right:
+        transform.position = transform.position + transform.right;
+        break;
+
+      case Direction.Left:
+        transform.position = transform.position - transform.right;
+        break;
+    }
+  }
+}
---
+++ Assets/Maze/Scripts/CharacterInput.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class CharacterInput : MonoBehaviour
+{
+  public CharacterControls controller;
+  void FixedUpdate()
+  {
+    var keyboard = Keyboard.current;
+    if (keyboard == null)
+      return; // No gamepad connected.
+
+    if (keyboard.wKey.IsPressed(1f))
+    {
+      controller.Move(Direction.Forward);
+    }
+    if (keyboard.sKey.IsPressed(1f))
+    {
+      controller.Move(Direction.Back);
+    }
+    if (keyboard.aKey.IsPressed(1f))
+    {
+      controller.Move(Direction.Left);
+    }
+    if (keyboard.dKey.IsPressed(1f))
+    {
+      controller.Move(Direction.Right);
+    }
+  }
+}
